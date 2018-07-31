bd7503e29a8a16bce64484a8a799a5ebc7b3ba74
Lisa Lehrman <lisa.lehrman@gmail.com>
2018-07-31 13:32:04 -0700

    updates to return max tone name and score

--- public/app.js
+++ public/app.js
@@ -8,14 +8,9 @@ function showNews(res) {
   for (i = 0; i < articles.length; i++) {
     let tone = articles[i].tone.document_tone.tone_categories[0].tones;
   //  console.log("tone is ", tone);
-   categoryList.push(tone);
-    // for (let j=0; j<articles.length; j++){
-    //   console.log(tone[i].score, tone[i].tone_name);
-    //   // console.log();
-    // }
+    categoryList.push(tone);
     let toneDescription = showTone(tone);
-    // console.log("toneDescription is ", toneDescription)
-
+    console.log("toneDescription is ", toneDescription);
     let articleTitle = articles[i].title;
     let imgSrc = articles[i].urlToImage;
     let articleDescription = articles[i].description;
@@ -43,12 +38,9 @@ function showNews(res) {
 };
 
 function showArticle(idx) {
-  // extract the article index from the data attribute
-  // attached to the article
-  // console.log("idx is ", idx);
+  // extract the article index from the data attribute attached to the article
   // use it to locate the article in global "articles" variable
   const article = global_articles[idx];
-
   // show the popup
   $("#popUp h1").text(article.title);
   $("#popUp p").text(article.description || "");
@@ -64,49 +56,31 @@ $.ajax({
   success: showNews
 });
 
-categoryList = []
-// console.log("global_articles: ", global_articles);
-// console.log("tone is ", tone);
-// categoryList.push(global_articles);
-console.log("categoryList is", categoryList);
-
-
-// categoryList.push({"tone_name":"anger",  "score":0.98});
-// categoryList.push({"tone_name":"disgust","score":0.99});
-// categoryList.push({"tone_name":"emo2",   "score":0.198});
-// categoryList.push({"tone_name":"emo3",   "score":0.598});
-// categoryList.push({"tone_name":"emo4",   "score":0.8798});
-
-// console.log(categoryList);
-/*
-var maxScore = categoryList[0].score;
-var maxName = categoryList[0].tone_name;
-for(var i=0;i<categoryList.length;i++){
-    if(categoryList[i].score >= maxScore){
-        maxName  = categoryList[i].tone_name;
-        maxScore = categoryList[i].score;
-    }
-}
-*/
-
-// categoryList.sort(function(tone1,tone2) {return tone2.score - tone1.score} );
-// maxName  = categoryList[0].tone_name
-// maxScore = categoryList[0].score
-// // console.log(maxScore + " - " + maxName);
+categoryList = [];
+categoryList.sort(function(tone1,tone2) {return tone2.score - tone1.score} );
 
 function showTone(categoryList){
-  let toneName = categoryList[0].tone_name;
-  let toneScore = categoryList[0].score;
-  // console.log("toneName is ", toneName);
-  // console.log("toneScore is ", toneScore);
+  let maxScore = categoryList[0].score;
+  let maxName = categoryList[0].tone_name;
+  for(var i=0; i<categoryList.length; i++){
+    // console.log("global articles titles", global_articles[i].title);
+    let toneName = categoryList[i].tone_name;
+    let toneScore = categoryList[i].score;
+    // console.log(toneName + " score is " + toneScore);
+    // console.log("is this happening?");
+    if(toneScore >= maxScore){
+      // console.log("is this happening?");
+    //   console.log("toneScore is ", toneScore);
+      maxName  = categoryList[i].tone_name;
+      maxScore = categoryList[i].score;
+      let maxScoreArr = [];
+      maxScoreArr.push({"maxName": maxName, "maxScore": maxScore});
+      // console.log("maxScoreArr is ", maxScoreArr);
+      console.log(maxName + " - " + maxScore);
+      // console.log("global articles is ", global_articles);
+    } 
+  };
+  
   return ''
-}
-
-
-/// get the name and score for each of the 5 tones. anger, disgust, etc. 
-// put them in an array [angerScore, disgustScore]
-// use the Math.max method on the array to return the biggest number
-
-
-
+};
 
